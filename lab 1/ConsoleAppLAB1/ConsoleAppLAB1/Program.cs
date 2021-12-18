c5a3fe2de11ad7aebba45c5225a127e1e47001d0
unknown <you@example.com>
2021-12-18 03:13:25 +0300

    Initial commit

---
+++ lab 1/ConsoleAppLAB1/ConsoleAppLAB1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLAB1
+{
+    class Program
+    {
+        public int a
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public int b
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public int c
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        class A
+        {
+
+        }
+        class B : A
+        {
+            public object a;
+            public object b;
+        }
+        class C : B
+        {
+            public object c;
+        }
+
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            A a3 = new A();
+            A a4 = new A();
+            B b5 = new B();
+            b5.a = a3;
+            b5.a = a4;
+
+            C c6 = new C();
+            c6.a = a1;
+            c6.a = a2;
+            c6.b = b5;
+
+        }
+    }
+}
